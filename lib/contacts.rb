9d81b98d251f72839526ce557acef7a4657f0200
Tiffany <ong.tifffany1503@gmail.com>
2019-08-08 18:50:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,5 +19,10 @@ def remove_strawberry(contacts)
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_icecream_flavors
         data.delete_if { |ice_cream| ice_cream == "strawberry" }
-
+      end
+    end
+  end
 end
+
+
+
